652ff9d511937e37ae70d6f6b81ebe63173f5cd5
Halann <hairulirawann@gmail.com>
2025-01-08 09:40:37 +0800

    Make the select option interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -61,20 +61,27 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
     const productId = button.dataset.productId;
 
+    const quantitySelector = document.querySelector(
+      `.js-quantity-selector-${productId}`
+    );
+
+    const quantity = Number(quantitySelector.value);
+
     let matchingItem;
 
     cart.forEach((item) => {
+      console.log(item);
       if (productId === item.productId) {
         matchingItem = item;
       }
     });
 
     if (matchingItem) {
-      matchingItem.quantity++;
+      matchingItem.quantity += quantity;
     } else {
       cart.push({
-        productId: productId,
-        quantity: 1,
+        productId,
+        quantity
       });
     }
 
