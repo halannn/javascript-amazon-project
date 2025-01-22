88353a1616b08f0b054c02cc52ff9ab2720f8104
Halann <hairulirawann@gmail.com>
2025-01-22 13:28:40 +0800

    Create automated tests for orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -30,7 +30,7 @@ export function renderOrderSummary() {
     const dateString = calculateDeliveryDate(deliveryOption);
 
     cartSummaryHTML += `
-      <div class="cart-item-container js-cart-item-container-${
+      <div class="cart-item-container js-cart-item-container js-cart-item-container-${
         matchingProduct.id
       }">
         <div class="delivery-date">
@@ -48,7 +48,7 @@ export function renderOrderSummary() {
             <div class="product-price">
             $${formatCurrency(matchingProduct.priceCents)}
             </div>
-            <div class="product-quantity">
+            <div class="product-quantity js-product-quantity-${matchingProduct.id}">
               <span>
                 Quantity: <span class="quantity-label">${
                   cartItem.quantity
@@ -65,7 +65,7 @@ export function renderOrderSummary() {
               <span class="save-quantity-link link-primary js-save-quantity-link" data-product-id="${
                 matchingProduct.id
               }">Save</span>
-              <span class="delete-quantity-link link-primary js-delete-quantity-link" data-product-id="${
+              <span class="delete-quantity-link link-primary js-delete-quantity-link js-delete-link-${matchingProduct.id}" data-product-id="${
                 matchingProduct.id
               }">
                 Delete
@@ -127,9 +127,9 @@ export function renderOrderSummary() {
       const productId = link.dataset.productId;
       removeFromCart(productId);
 
-      updateCartQuantity();
+      // updateCartQuantity();
       renderOrderSummary();
-      renderPaymentSummary();
+      // renderPaymentSummary();
     });
   });
 
@@ -141,7 +141,7 @@ export function renderOrderSummary() {
     ).innerHTML = `${cartQuantity} items`;
   }
 
-  updateCartQuantity();
+  // updateCartQuantity();
 
   document.querySelectorAll(".js-update-quantity-link").forEach((link) => {
     link.addEventListener("click", () => {
