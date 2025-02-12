80618a60540f1d980932f49e7e95a73f4e8fa4b7
Halann <hairulirawann@gmail.com>
2025-02-12 08:58:27 +0800

    Create automated test for formatCurrency

--- tests/utils/moneyTest.js
+++ tests/utils/moneyTest.js
@@ -11,5 +11,10 @@ describe("test suite : formatCurrency", () => {
 
   it("rounds up to the the nearest cent", () => {
     expect(formatCurrency(2000.5)).toEqual("20.01");
+    expect(formatCurrency(2000.4)).toEqual("20.00");
   });
+
+  it('testing negative numbers',()=>{
+    expect(formatCurrency(-1000)).toEqual('-10.00')
+  })
 });
